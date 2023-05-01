3ae35947f9d7db8a5f143500399b224c0bcbcf3b
ZengChanghuan <zengchanghuan@gmail.com>
2023-05-01 20:23:48 +0800

    add chat widget

--- chat_app/lib/screen/chat_screen.dart
+++ chat_app/lib/screen/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/constants/constants.dart';
 import 'package:chat_app/services/assets_manager.dart';
+import 'package:chat_app/widget/chat_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
@@ -51,7 +52,10 @@ class _ChatScreenState extends State<ChatScreen> {
               child: ListView.builder(
                   itemCount: 5,
                   itemBuilder: (context, index) {
-                    return const Text("hello this is a txt");
+                    return  ChatWidget(
+                      msg: chatMessages[index]["msg"].toString(),
+                      chatIndex: int.parse(chatMessages[index]["chatIndex"].toString()),
+                    );
                   }),
             ),
             if (_isTyping) ...[
