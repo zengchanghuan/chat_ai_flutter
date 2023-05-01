cb6678559d3f1696767d4d9bc309fe88087b94a8
ZengChanghuan <zengchanghuan@gmail.com>
2023-05-01 16:27:46 +0800

    add flutter project

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:chat_app/screen/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'constants/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter ChatBOT',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          scaffoldBackgroundColor: scaffoldBackgroundColor,
+          appBarTheme: AppBarTheme(
+            color: cardColor,
+          )),
+      home: const ChatScreen(),
+    );
+  }
+}
+
---
+++ chat_app/lib/screen/chat_screen.dart
@@ -0,0 +1,20 @@
+import 'package:chat_app/services/assets_manager.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(AssetsManager.openaiLogo),
+      ),
+    );
+  }
+}
